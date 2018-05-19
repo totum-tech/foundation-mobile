495048f60abccc27d67a0367782204ca86bddc0c
Marcus Bernales <marcusbernales@gmail.com>
2018-05-18 23:27:59 -0700

    Added native base, setup a common layout with header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { compose, lifecycle, withStateHandlers } from 'recompose'
 import { StyleSheet, Text, View } from 'react-native'
+import { Header, Container, Left, Right, Title, Body } from 'native-base'
 import { NativeRouter, Route } from 'react-router-native'
+import Expo from 'expo'
 
 const styles = StyleSheet.create({
   container: {
@@ -19,16 +22,37 @@ const Home = () => (
   </View>
 )
 
-const App = () => (
+const App = ({ loaded }) => (
   <NativeRouter>
-    <View style={styles.container}>
-      <Route
-        exact
-        path="/"
-        component={Home}
-      />
-    </View>
+    {loaded ? (
+      <Container>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Foundation</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Body>
+          <Route exact path="/" component={Home} />
+        </Body>
+      </Container>
+    ) : (
+      <View />
+    )}
   </NativeRouter>
 )
 
-export default App
+export default compose(
+  withStateHandlers({ loaded: false }, { setLoaded: () => loaded => ({ loaded }) }),
+  lifecycle({
+    componentWillMount() {
+      Expo.Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      }).then(() => {
+        this.props.setLoaded(true)
+      })
+    },
+  }),
+)(App)
